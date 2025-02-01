103ad59f1975213a51cafdf33748809f8acaab51
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-01 13:21:20 +0000

    Update env.py and settings.py prior to Heroku re-deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c%n6oh*&-6zn5(s4wakk^_2g!epy&_jcq8nfr7p_@ymf9)k@we'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
