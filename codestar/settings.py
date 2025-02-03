f5f212aff32673c0c6831473d0f52c2b39a6503b
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-03 13:04:12 +0000

    Update models.py in blog folder

--- codestar/settings.py
+++ codestar/settings.py
@@ -86,10 +86,14 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
